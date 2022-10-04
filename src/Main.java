3dcf0bc7e1f16df491849d3020041152dcb31e07
Smile <smailbestybay@gmail.com>
2022-10-03 19:54:36 -0700

    in-progress searchCSV, can find row and working on title

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
-/**
- * 
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
 /**
- * @author smile
+ * @author Smail, Dre
  *
  */
 public class Main
@@ -14,8 +14,58 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		// TODO Auto-generated method stub
+		//
+		// TODO Open CSV file and print first row.
+		searchCSV(10);
 
+	}
+	
+	public static String searchCSV(int row)
+	{
+		File inFile = new File ( "top200GlobalCharts.csv" );
+		
+		
+		Scanner rowSc = null;
+		Scanner colSc = null;
+		try
+		{
+			rowSc = new Scanner (inFile);
+			int count = 0;
+			while (rowSc.hasNextLine() && count <= row)
+			{
+				String line = rowSc.nextLine();
+				
+				// search line
+				// https://www.youtube.com/watch?v=1AyhR5A6ln4&t=231s&ab_channel=DevNami
+				// gonna need to use delimiter
+				
+				if (count == row)
+				{
+					System.out.println(line);
+					colSc = new Scanner(line);
+					colSc.useDelimiter(",");
+					while (colSc.hasNext())
+					{
+						line = colSc.next();
+						if(line.contains("I Feel It Coming")) {
+							System.out.println(line);
+							return line;
+						}
+					}
+				}
+				count++;
+			}
+		}
+		catch (FileNotFoundException e)
+		{
+			e.printStackTrace();
+		}
+		finally 
+		{
+			rowSc.close();
+			colSc.close();
+		}
+		return "No results";
 	}
 
 }
