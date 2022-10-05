e19af613e9b7e028db36968455d3aa47871e6d30
Tyler Andre <tandre_2@live.com>
2022-10-05 12:07:50 -0700

    Added Person Class

---
+++ src/Person.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ * @author Dre, Smail 
+ * Represents a person. A person has a name. 
+ */
+public class Person
+{
+	//Fields
+	private String name;
+	
+	//Constructors
+	
+	/**
+	 * Default constructor for a Person
+	 */
+	public Person(String name)
+	{
+		this.name = name;
+	}
+}
