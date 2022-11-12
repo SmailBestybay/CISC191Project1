958ea78a311b48a4dac053dea7bfbc1b2570d298
Smile <smailbestybay@gmail.com>
2022-11-12 00:32:20 -0800

    start GUI, stop at logged out state in center zone

---
+++ src/GUI.java
@@ -0,0 +1,73 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class GUI extends JFrame {
+
+    private static final String APP_NAME = "Spotify Super Lite!";
+    private JPanel body;
+    private JPanel navbar;
+    private JPanel logInPanel;
+    private JLabel appName;
+
+    // Logged out state components
+    private JLabel userNameFieldLabel;
+
+
+    /**
+     * GUI constructor
+     * add all panels to the JFrame, start in Logged out state.
+     */
+    public GUI(){
+        super(GUI.APP_NAME);
+
+        makeBody();
+
+        add(body);
+
+        setSize(400,400);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
+
+    /**
+     * make body JPanel.
+     * start in Logged out state.
+     * add navbar and loginPanel panels.
+     */
+    private void makeBody() {
+        body = new JPanel(new BorderLayout());
+        makeNavbar();
+        makeLogInPanel();
+        body.add(navbar, BorderLayout.NORTH);
+        body.add(logInPanel, BorderLayout.CENTER);
+
+
+    }
+
+    /**
+     * make navbar JPanel.
+     * start in Logged out state.
+     *
+     */
+    private void makeNavbar() {
+        navbar = new JPanel();
+        appName = new JLabel(GUI.APP_NAME);
+        appName.setFont(new Font(Font.SANS_SERIF, Font.ITALIC, 20));
+        navbar.add(appName);
+    }
+
+    private void makeLogInPanel() {
+        logInPanel = new JPanel();
+        logInPanel.setLayout(new BoxLayout(logInPanel, BoxLayout.Y_AXIS));
+        userNameFieldLabel = new JLabel("User Name");
+
+        // centers label
+        userNameFieldLabel.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        // adds space to the top of the center panel
+        logInPanel.add(Box.createRigidArea(new Dimension(0,20)));
+
+        logInPanel.add(userNameFieldLabel);
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import javax.swing.*;
 import java.util.ArrayList;
 
 /**
@@ -14,85 +15,85 @@ public class Main
 	{
 		SpotifyDatabase db = new SpotifyDatabase("dataBase.csv");
 		User user = null;
+		GUI view = new GUI();
+
 		// Greet user, then login or register user
-		try {
-			ConsoleUI.greetings();
-
-			while(user == null)
-			{
-				if(ConsoleUI.promptToShowExistingUsers())
-				{
-					ConsoleUI.showExistingUsers();
-				}
-				user = ConsoleUI.login();
-				if (user == null)
-				{
-					// register user
-					user = ConsoleUI.register();
-				} else {
-
-					// Enter standby mode:
-					while(user != null)
-					{
-						System.out.println("---------------------------------------------------------");
-						System.out.print("Current User: ");
-						System.out.print(user.getName() + "\n");
-
-						// Listen to commands from user.
-						System.out.println("Standing by for a command...");
-						System.out.println("Enter \"help\" for list of available commands");
-						String command = ConsoleUI.standByMode();
-
-						/////////////////// if statement for every command ///////////////////
-
-						switch (command) {
-							case "help" -> ConsoleUI.listCommands();
-							case "error" -> System.out.println("ERROR: command must be only one word.");
-							case "logout" -> user = null;
-							case "search" -> {
-								switch (ConsoleUI.searchMethod()) {
-									case "error" -> System.out.println("ERROR: search method undefined");
-									case "artist" -> {
-										ArrayList<Artist> artists = db.searchArtist(ConsoleUI.searchByArtists());
-										user.addArtist(ConsoleUI.displayArtistResults(artists));
-										userDatabase.exportCSV(user);
-									}
-									case "song" -> {
-										ArrayList<Song> songs = db.searchSong(ConsoleUI.searchBySong());
-										user.addSong(ConsoleUI.displaySongResults(songs));
-										userDatabase.exportCSV(user);
-									}
-									case "mixed" -> {
-										ConsoleUI.search();
-									}
-								}
-							}
-							case "showAll" -> ConsoleUI.showFavorites(user.getAll());
-							case "remove" -> {
-								Object object = ConsoleUI.removeEntry(user.getAll());
-								if(object instanceof Song)
-								{
-									user.removeSong((Song) object);
-								}
-								if(object instanceof Artist)
-								{
-									user.removeArtist((Artist) object);
-								}
-								userDatabase.exportCSV(user);
-								System.out.println("Entry removed successfully.");
-							}
-							case "shutdown" -> System.exit(0);
-							default -> System.out.println("Command does not exist.");
-						}
-
-						//////////////// End of if statement for every command /////////////////
-					}
-				}
-			}
-		}
-		catch (InterruptedException | NullPointerException e)
-		{
-			e.printStackTrace();
-		}
+//		try {
+//			ConsoleUI.greetings();
+//
+//			while(user == null)
+//			{
+//				if(ConsoleUI.promptToShowExistingUsers())
+//				{
+//					ConsoleUI.showExistingUsers();
+//				}
+//				user = ConsoleUI.login();
+//				if (user == null)
+//				{
+//					// register user
+//					user = ConsoleUI.register();
+//				} else {
+//
+//					// Enter standby mode:
+//					while(user != null)
+//					{
+//						System.out.println("---------------------------------------------------------");
+//						System.out.print("Current User: ");
+//						System.out.print(user.getName() + "\n");
+//
+//						// Listen to commands from user.
+//						System.out.println("Standing by for a command...");
+//						System.out.println("Enter \"help\" for list of available commands");
+//						String command = ConsoleUI.standByMode();
+//
+//						/////////////////// if statement for every command ///////////////////
+//
+//						switch (command) {
+//							case "help" -> ConsoleUI.listCommands();
+//							case "error" -> System.out.println("ERROR: command must be only one word.");
+//							case "logout" -> user = null;
+//							case "search" -> {
+//								switch (ConsoleUI.searchMethod()) {
+//									case "error" -> System.out.println("ERROR: search method undefined");
+//									case "artist" -> {
+//										ArrayList<Artist> artists = db.searchArtist(ConsoleUI.searchByArtists());
+//										user.addArtist(ConsoleUI.displayArtistResults(artists));
+//										userDatabase.exportCSV(user);
+//									}
+//									case "song" -> {
+//										ArrayList<Song> songs = db.searchSong(ConsoleUI.searchBySong());
+//										user.addSong(ConsoleUI.displaySongResults(songs));
+//										userDatabase.exportCSV(user);
+//									}
+//									case "mixed" -> ConsoleUI.search();
+//								}
+//							}
+//							case "showAll" -> ConsoleUI.showFavorites(user.getAll());
+//							case "remove" -> {
+//								Object object = ConsoleUI.removeEntry(user.getAll());
+//								if(object instanceof Song)
+//								{
+//									user.removeSong((Song) object);
+//								}
+//								if(object instanceof Artist)
+//								{
+//									user.removeArtist((Artist) object);
+//								}
+//								userDatabase.exportCSV(user);
+//								System.out.println("Entry removed successfully.");
+//							}
+//							case "shutdown" -> System.exit(0);
+//							default -> System.out.println("Command does not exist.");
+//						}
+//
+//						//////////////// End of if statement for every command /////////////////
+//					}
+//				}
+//			}
+//		}
+//		catch (InterruptedException | NullPointerException e)
+//		{
+//			e.printStackTrace();
+//		}
 	}
 }
