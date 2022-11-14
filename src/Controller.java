7fb7447cdf0191bca04d480525adce0d9cfc78c1
Smile <smailbestybay@gmail.com>
2022-11-14 02:07:48 -0800

    implement login button logs in user

--- src/Controller.java
+++ src/Controller.java
@@ -3,23 +3,31 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 public class Controller {
-    private SpotifyDatabase db;
-    private GUI gui;
-    private User currentUser;
+    private static SpotifyDatabase db;
+    private static GUI gui;
+    private static User currentUser;
 
-    public Controller(SpotifyDatabase db, GUI gui) {
-        this.db = db;
-        this.gui = gui;
+    public Controller(SpotifyDatabase model, GUI view) {
+        db = model;
+        gui = view;
+        currentUser = null;
 
-        gui.getLogInPanel().getLoginButton().addActionListener(new LogInButtonListener());
+        JTextField userNameField = gui.getLogInPanel().getUserNameField();
+        gui.getLogInPanel().getLoginButton().addActionListener(new LogInButtonListener(userNameField));
     }
 
     static class LogInButtonListener implements ActionListener {
 
-        private JButton loginButton;
+        private JTextField userNameField;
+
+        public LogInButtonListener(JTextField userNameField) {
+            this.userNameField = userNameField;
+        }
         @Override
         public void actionPerformed(ActionEvent e) {
             System.out.println("Login button pressed!");
+            currentUser = userDatabase.importUser(userNameField.getText());
+            gui.updateBody(currentUser);
         }
     }
 }
