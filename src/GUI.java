e832803c4546ce7df5da2b3b038272eb3aeec797
Smile <smailbestybay@gmail.com>
2022-11-14 01:03:25 -0800

    refactor GUI class to implement inner classes

--- src/GUI.java
+++ src/GUI.java
@@ -1,35 +1,15 @@
 import javax.swing.*;
 import java.awt.*;
-import java.util.Arrays;
 
+/**
+ * GUI class, uses multiple nested inner classes to organise fields
+ */
 public class GUI extends JFrame {
-
     private static final String APP_NAME = "Spotify Super Lite!";
     private JPanel body;
     private Navbar navbar;
-    private JPanel logInPanel;
-    private JPanel contentPanel;
-
-    // Logged out state center components, LogInPanel
-    private JLabel userNameFieldLabel;
-    private JTextField userNameField;
-    private JButton loginButton;
-    private JButton registerButton;
-    private JLabel messageLabel;
-
-    // Content panel components
-    private JPanel searchPanel;
-    private JPanel itemsPanel;
-
-    // searchPanel components
-    private JLabel searchLabel;
-    private JTextField searchField;
-    private JButton searchButton;
-    private JComboBox<String> searchMode;
-
-    // itemsPanel components
-    private SongItem[] songItems;
-
+    private LogInPanel logInPanel;
+    private ContentPanel contentPanel;
 
     /**
      * GUI constructor
@@ -47,7 +27,7 @@ public class GUI extends JFrame {
         pack();
         setVisible(true);
 
-//        updateBody(new User("Smile"));
+        updateBody(new User("Smile"));
 
     }
 
@@ -59,11 +39,15 @@ public class GUI extends JFrame {
     private void makeBody() {
         body = new JPanel(new BorderLayout());
         navbar = new Navbar();
-        makeLogInPanel();
+        logInPanel = new LogInPanel();
         body.add(navbar, BorderLayout.NORTH);
         body.add(logInPanel, BorderLayout.CENTER);
     }
 
+    /**
+     * update body panel
+     * @param user user data
+     */
     public void updateBody(User user) {
 
         if (user == null){
@@ -75,87 +59,77 @@ public class GUI extends JFrame {
         if (user != null) {
             navbar.updateNavbar(user);
             body.remove(logInPanel);
-            makeContentPanel();
+            contentPanel = new ContentPanel();
             body.add(contentPanel, BorderLayout.CENTER);
         }
         pack();
     }
 
-    private void makeLogInPanel() {
-        logInPanel = new JPanel();
-        logInPanel.setLayout(new BoxLayout(logInPanel, BoxLayout.Y_AXIS));
+    /**
+     * content panel inner class
+     */
+    class ContentPanel extends JPanel {
+        private SearchPanel searchPanel;
+        private ItemsPanel itemsPanel;
+        public ContentPanel(){
+            super();
+            setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+            searchPanel = new SearchPanel();
+            itemsPanel = new ItemsPanel();
 
-        // instantiate components of this panel
-        userNameFieldLabel = new JLabel("User Name");
-        userNameField = new JTextField(2);
-        userNameField.setMaximumSize(new Dimension(200, 26));
-        loginButton = new JButton("Log in");
-        registerButton = new JButton("Register");
-        messageLabel = new JLabel();
-        messageLabel.setText("Welcome! Please Log in or Register");
+            centerWidget(searchPanel);
+            centerWidget(itemsPanel);
 
-        // center components
-        centerWidget(userNameFieldLabel);
-        centerWidget(userNameField);
-        centerWidget(loginButton);
-        centerWidget(registerButton);
-        centerWidget(messageLabel);
+            add(searchPanel);
+            add(itemsPanel);
+        }
 
-        // adds space to the top of the center panel
-        logInPanel.add(Box.createRigidArea(new Dimension(0,100)));
+        /**
+         * search panel inner class
+         */
+        class SearchPanel extends JPanel {
+            private JLabel searchLabel;
+            private JTextField searchField;
+            private JButton searchButton;
+            private JComboBox<String> searchMode;
 
-        // add components
-        logInPanel.add(userNameFieldLabel);
-        logInPanel.add(userNameField);
-        logInPanel.add(loginButton);
-        logInPanel.add(registerButton);
-        logInPanel.add(Box.createRigidArea(new Dimension(0,50)));
-        logInPanel.add(messageLabel);
+            /**
+             * Search Panel constructor
+             */
+            public SearchPanel(){
+                super();
 
-        // add space to the bot of the center panel
-        logInPanel.add(Box.createRigidArea(new Dimension(0,100)));
-    }
+                // components
+                searchLabel = new JLabel("Search for a song or an artist");
+                searchField = new JTextField(10);
+                searchButton = new JButton("Search");
+                String[] searchOptions = {"Song", "Artist", "Mixed"};
+                searchMode = new JComboBox<>(searchOptions);
 
-    private void makeContentPanel(){
-        contentPanel = new JPanel();
-        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
-        makeSearchPanel();
-        makeItemsPanel();
+                add(searchLabel);
+                add(searchField);
+                add(searchButton);
+                add(searchMode);
 
-        centerWidget(searchPanel);
-        centerWidget(itemsPanel);
+            }
+        }
 
-        contentPanel.add(searchPanel);
-        contentPanel.add(itemsPanel);
-    }
+        /**
+         * Items panel, responsible for displaying query results.
+         */
+        class ItemsPanel extends JPanel {
 
-    private void makeSearchPanel(){
-        searchPanel = new JPanel();
-
-        // components
-        searchLabel = new JLabel("Search for a song or an artist");
-        searchField = new JTextField(10);
-        searchButton = new JButton("Search");
-        String[] searchOptions = {"Song", "Artist", "Mixed"};
-        searchMode = new JComboBox<>(searchOptions);
-
-        searchPanel.add(searchLabel);
-        searchPanel.add(searchField);
-        searchPanel.add(searchButton);
-        searchPanel.add(searchMode);
-
-    }
-    private void makeItemsPanel() {
-        itemsPanel = new JPanel(new GridLayout(0,1));
+            private SongItem[] songItems;
+            public ItemsPanel() {
+                super();
+                this.setLayout(new GridLayout(0,1));
+            }
+        }
     }
 
     /**
-     * Helper method to center a component.
-     * @param component center the component
+     * Navbar inner class
      */
-    private static void centerWidget(JComponent component){
-        component.setAlignmentX(Component.CENTER_ALIGNMENT);
-    }
     class Navbar extends JPanel {
         // Navbar components
         private JLabel appName;
@@ -216,6 +190,56 @@ public class GUI extends JFrame {
             pack();
         }
     }
+
+    /**
+     * Login panel inner class
+     */
+    class LogInPanel extends JPanel {
+        private JLabel userNameFieldLabel;
+        private JTextField userNameField;
+        private JButton loginButton;
+        private JButton registerButton;
+        private JLabel messageLabel;
+
+        public LogInPanel() {
+            super();
+            this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+
+            // instantiate components of this panel
+            userNameFieldLabel = new JLabel("User Name");
+            userNameField = new JTextField(2);
+            userNameField.setMaximumSize(new Dimension(200, 26));
+            loginButton = new JButton("Log in");
+            registerButton = new JButton("Register");
+            messageLabel = new JLabel();
+            messageLabel.setText("Welcome! Please Log in or Register");
+
+            // center components
+            centerWidget(userNameFieldLabel);
+            centerWidget(userNameField);
+            centerWidget(loginButton);
+            centerWidget(registerButton);
+            centerWidget(messageLabel);
+
+            // adds space to the top of the center panel
+            add(Box.createRigidArea(new Dimension(0,100)));
+
+            // add components
+            add(userNameFieldLabel);
+            add(userNameField);
+            add(loginButton);
+            add(registerButton);
+            add(Box.createRigidArea(new Dimension(0,50)));
+            add(messageLabel);
+
+            // add space to the bot of the center panel
+            add(Box.createRigidArea(new Dimension(0,100)));
+        }
+    }
+
+    /**
+     * Song item inner class
+     */
     class SongItem extends JPanel {
         private JLabel title;
         private JLabel rank;
@@ -244,4 +268,11 @@ public class GUI extends JFrame {
         }
     }
 
+    /**
+     * Helper method to center a component.
+     * @param component center the component
+     */
+    private static void centerWidget(JComponent component){
+        component.setAlignmentX(Component.CENTER_ALIGNMENT);
+    }
 }
